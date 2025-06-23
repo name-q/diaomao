56121ef80e8a53025c0068be9f8f5968e48d5366
name-q <1620206666@qq.com>
2025-06-23 17:05:56 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'eslint-config-egg',
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // 自定义规则
+  },
+};
